7f50bda6b0247f4d6507371343ba34a1126c1493
1327wukai <wukai091@foxmail.com>
2022-06-27 15:04:59 +0800

    oa

--- springboot-demo/src/main/java/com/example/springbootdemo/controller/AttendanceController.java
+++ springboot-demo/src/main/java/com/example/springbootdemo/controller/AttendanceController.java
@@ -5,7 +5,6 @@ import com.example.springbootdemo.serivce.AttendanceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
